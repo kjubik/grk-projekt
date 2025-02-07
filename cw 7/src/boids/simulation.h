780ec90484a2885e58d8df9a1a4bc11ae174748d
Maciej Kmieć <97192632+starswaterbrook@users.noreply.github.com>
2025-02-07 16:17:54 +0100

    feat: boid implementation with bounding box (#2)
    * feat: working boid implementation with bounding box
    
    * chore: solution files fix, remove unnecessary util functions
    
    * chore: ide solution view fix
    
    * chore: move boid update call to the render loop
    
    * chore: separate boid related file structure
    
    * fix: correct import paths for moved boids files
    
    * chore: remove unused utility function

---
+++ cw 7/src/boids/simulation.h
@@ -0,0 +1,32 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+struct SimulationParams {
+    float avoidRadius;
+    float avoidForce;
+    float alignRadius;
+    float alignForce;
+    float cohesionRadius;
+    float cohesionForce;
+    float deltaTime;
+    int boidNumber;
+    float boundMin;
+    float boundMax;
+    float bounceForce;
+
+    SimulationParams(float avoidR = 1.2f, float avoidF = 2.0f,
+        float alignR = 1.5f, float alignF = 0.8f,
+        float cohesionR = 2.0f, float cohesionF = 0.4f,
+        float boundMn = -8.0f, float boundMx = 8.0f,
+        float bounceF = 1.5f, int boidNum = 200,
+        float dt = 0.05)
+        : avoidForce(avoidF), avoidRadius(avoidR),
+        alignForce(alignF), alignRadius(alignR),
+        cohesionForce(cohesionF), cohesionRadius(cohesionR),
+        boundMin(boundMn), boundMax(boundMx),
+        bounceForce(bounceF), boidNumber(boidNum),
+        deltaTime(dt) {
+    }
+};
+
+#endif //WIDGET_H
