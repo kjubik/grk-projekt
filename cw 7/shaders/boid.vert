19c35fe616cab999b4a397b649992d693db4414b
Maciej Kmieć <97192632+starswaterbrook@users.noreply.github.com>
2025-02-08 17:44:54 +0100

    feat: assimp-based boid rendering (#3)
    * feat: modify boid and flock classes to render imported model meshes
    
    * feat: render tree object, use bird object for boids

--- cw 7/shaders/boid.vert
+++ cw 7/shaders/boid.vert
@@ -1,8 +1,9 @@
 #version 410 core
 layout (location = 0) in vec3 position;
-layout (location = 1) in vec3 color;
 
 out vec3 fragColor;
+
+uniform vec3 inputColor;  // Add this
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,5 +12,5 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(position, 1.0);
-    fragColor = color;
+    fragColor = inputColor;
 }
--- cw 7/src/boids/simulation.h
+++ cw 7/src/boids/simulation.h
@@ -17,19 +17,20 @@ struct SimulationParams {
     float boundMin;
     float boundMax;
     float bounceForce;
+    float boidModelScale;
 
     SimulationParams(float avoidR = 1.2f, float avoidF = 2.0f,
         float alignR = 1.5f, float alignF = 0.8f,
         float cohesionR = 2.0f, float cohesionF = 0.4f,
         float boundMn = -8.0f, float boundMx = 8.0f,
         float bounceF = 1.5f, int boidNum = 200,
-        float dt = 0.05)
+        float dt = 0.05, float scale = 0.015f)
         : avoidForce(avoidF), avoidRadius(avoidR),
         alignForce(alignF), alignRadius(alignR),
         cohesionForce(cohesionF), cohesionRadius(cohesionR),
         boundMin(boundMn), boundMax(boundMx),
         bounceForce(bounceF), boidNumber(boidNum),
-        deltaTime(dt) {
+        deltaTime(dt), boidModelScale(scale) {
     }
 };
 
