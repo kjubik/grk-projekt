48e02ebdf4126d7d822a18559a6f962628fd202c
Wojciech Kubicki <wojt.kubicki@gmail.com>
2025-02-10 01:18:36 +0100

    feat: change terrain texture to rocky for more visible normal mapping effect

--- cw 7/src/ex_7_1.hpp
+++ cw 7/src/ex_7_1.hpp
@@ -324,7 +324,7 @@ void init(GLFWwindow* window)
 		gradientTextures[i] = Core::LoadTexture(texturePath.c_str());
 	}
 
-	terrainTexture = Core::LoadTexture("textures/terrain/grass3.jpg");
+	terrainTexture = Core::LoadTexture("textures/terrain/rocky.jpg");
 	terrainNormal = Core::LoadTexture("textures/terrain/normal.jpg");
 
 	setupBoidVAOandVBO(boidVAO, boidVBO, boidVertices, sizeof(boidVertices));
